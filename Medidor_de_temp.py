fb5381968a302177f28f83922065567cf3a7d0f4
Victor Arruda <victorcordeiroarruda@gmail.com>
2021-03-23 08:08:34 -0300

    aumento na taxa de atualização para 100 ms

--- Medidor_de_temp.py
+++ Medidor_de_temp.py
@@ -7,17 +7,18 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.patches as mpl_patches
 
 
-def insere_legendas(temp_min, temp_media, temp_max):
+def insere_legendas(temp_min, temp_media, temp_max, temp_actual):
     #Create the legend without superposing the data in the plot :D
 
     # create a list with two empty handles (or more if needed)
-    handles = [mpl_patches.Rectangle((0, 0), 1, 1, fc="white", ec="white", lw=0, alpha=0)] * 3
+    handles = [mpl_patches.Rectangle((0, 0), 1, 1, fc="white", ec="white", lw=0, alpha=0)] * 4
 
     # create the corresponding number of labels (= the text you want to display)
     labels = []
     labels.append("máx =  %.2f ºC" %temp_max)
     labels.append("média = %.2f ºC" %temp_media)
     labels.append("mín = %.2f ºC" %temp_min)
+    labels.append("actual = %.2f ºC" %temp_actual)
 
     # create the legend, supressing the blank space of the empty line symbol and the
     # padding between symbol and label by setting handlelenght and handletextpad
@@ -51,18 +52,19 @@ def animate(i):
         temp_min = min(temperature)
         temp_max = max(temperature)
         temp_media = sum(temperature)/len(temperature)
+        temp_actual = temperature[-1]
         plt.cla()
-        plt.title('Temperatura do quarto x tempo')
+        plt.title('Temperatura x tempo')
         plt.xlabel("Tempo (min)")
         plt.ylabel("Temperatura (ºC)")
-        insere_legendas(temp_min, temp_media, temp_max)
+        insere_legendas(temp_min, temp_media, temp_max, temp_actual)
 
         #converte para minutos
-        x_min = [float(x)/ 120 for x in x_vals]
+        x_min = [float(x)/ 600 for x in x_vals]
         plt.plot(x_min, temperature)
 
        
-ani = FuncAnimation(plt.gcf(), animate, interval = 500)
+ani = FuncAnimation(plt.gcf(), animate, interval = 100)
 
 plt.tight_layout()
 plt.show()
